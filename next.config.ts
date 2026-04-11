e1fe22550c008b241f5087d04946ed892e10205a
Matheus Santos <124845403+matxxs@users.noreply.github.com>
2026-04-11 08:17:56 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
-// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Change 'standalone' to 'export' for GitHub Pages
-  /* config options here */
+  output: 'export', // Use 'export' for GitHub Pages, not 'standalone'
+  images: {
+    unoptimized: true, // Necessary for static exports on GitHub Pages
+  },
 };
 
 export default nextConfig;
