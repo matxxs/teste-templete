f1eef1c096dd79454c8aeaea91a325eebb90ae96
Matheus Santos <matheusdossantosmoura64@gmail.com>
2026-04-11 08:55:34 -0300

    correcao

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // Use 'export' for GitHub Pages, not 'standalone'
-  images: {
-    unoptimized: true, // Necessary for static exports on GitHub Pages
-  },
-};
-
-export default nextConfig;
