8e00bba8c04b7bfa1a0aa3f9dc87ee1f9827cb37
Matheus Santos <124845403+matxxs@users.noreply.github.com>
2026-04-11 08:16:15 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "standalone",
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // Change 'standalone' to 'export' for GitHub Pages
   /* config options here */
 };
 
